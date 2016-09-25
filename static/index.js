2f2fc2650875fe66100b643ac2a44dbbf1ab0f96
Enigmamemory <enigmamemory@gmail.com>
2016-09-25 11:37:07 -0400

    Dealing with clicking circles

--- static/index.js
+++ static/index.js
@@ -8,21 +8,25 @@ $(document).ready(function(){
     $("#button1").click(function(){
 	var div1 = document.createElement('div'); /*create div*/
 	div1.id = "circle_lv1_tl"; /*defines div's class*/
+	div1.style.backgroundColor = "#FFFFFF";
 	div1.onclick = Clicktl();
 	document.body.appendChild(div1); /*adds div*/
 
 	var div2 = document.createElement('div'); /*create div*/
 	div2.id = "circle_lv1_tr"; /*defines div's class*/
+	div2.style.backgroundColor = "#FFFFFF";
 	div2.onclick = Clicktr();
 	document.body.appendChild(div2); /*adds div*/
 
 	var div3 = document.createElement('div'); /*create div*/
 	div3.id = "circle_lv1_bl"; /*defines div's class*/
+	div3.style.backgroundColor = "#FFFFFF";
 	div3.onclick = Clickbl();
 	document.body.appendChild(div3); /*adds div*/
 
 	var div4 = document.createElement('div'); /*create div*/
 	div4.id = "circle_lv1_br"; /*defines div's class*/
+	div4.style.backgroundColor = "#FFFFFF";
 	div4.onclick = Clickbr();
 	document.body.appendChild(div4); /*adds div*/
 	
