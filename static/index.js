43358edd763011d3bcc8bd23dacbf15f3f8e1a0c
Enigmamemory <enigmamemory@gmail.com>
2016-09-25 03:20:21 -0400

    Created button, press should load game

--- static/index.js
+++ static/index.js
@@ -5,15 +5,15 @@ $(document).ready(function(){
 	document.body.appendChild(div1); /*adds div*/
 
 	var div2 = document.createElement('div'); /*create div*/
-	div2.id = "circle_lv1_tl"; /*defines div's class*/
+	div2.id = "circle_lv1_tr"; /*defines div's class*/
 	document.body.appendChild(div1); /*adds div*/
 
 	var div3 = document.createElement('div'); /*create div*/
-	div3.id = "circle_lv1_tl"; /*defines div's class*/
+	div3.id = "circle_lv1_bl"; /*defines div's class*/
 	document.body.appendChild(div1); /*adds div*/
 
 	var div4 = document.createElement('div'); /*create div*/
-	div4.id = "circle_lv1_tl"; /*defines div's class*/
+	div4.id = "circle_lv1_br"; /*defines div's class*/
 	document.body.appendChild(div1); /*adds div*/
 	
 	var element = document.getElementById("button1"); /*finds button*/
