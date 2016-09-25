afa8751c97acc084adcc8c486683c05846a597b8
Enigmamemory <enigmamemory@gmail.com>
2016-09-25 09:30:10 -0400

    Randomness error checking

--- static/index.js
+++ static/index.js
@@ -4,18 +4,22 @@ $(document).ready(function(){
     $("#button1").click(function(){
 	var div1 = document.createElement('div'); /*create div*/
 	div1.id = "circle_lv1_tl"; /*defines div's class*/
+	div1.background = "#FFFFFF";
 	document.body.appendChild(div1); /*adds div*/
 
 	var div2 = document.createElement('div'); /*create div*/
 	div2.id = "circle_lv1_tr"; /*defines div's class*/
+	div2.background = "#FFFFFF";
 	document.body.appendChild(div2); /*adds div*/
 
 	var div3 = document.createElement('div'); /*create div*/
 	div3.id = "circle_lv1_bl"; /*defines div's class*/
+	div3.background = "#FFFFFF";
 	document.body.appendChild(div3); /*adds div*/
 
 	var div4 = document.createElement('div'); /*create div*/
 	div4.id = "circle_lv1_br"; /*defines div's class*/
+	div4.background = "#FFFFFF";
 	document.body.appendChild(div4); /*adds div*/
 	
 	var element = document.getElementById("button1"); /*finds button*/
